de479ed04faa2714bcba66fb9079a2b0edcdfc16
Travis Grathwell <tjgrathwell@gmail.com>
2016-01-04 23:41:18 -0800

    Keep the 'progress' formatter enabled alongside 'jasmine-diff...' formatter
    Otherwise the "Test 1 of 10" kinda stuff doesn't show up on
    the console while tests are running.

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     basePath: '.',
     frameworks: ['jasmine'],
-    reporters: 'jasmine-diff',
+    reporters: ['jasmine-diff', 'progress'],
     jasmineDiffReporter: {
       pretty: true
     },
